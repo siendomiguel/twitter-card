3c8fe3ff5151a59ee70ef77e5b9dcc4bf1355f07
siendomiguel <siendomigueldev@gmail.com>
2022-07-22 12:11:55 -0500

    maquetacion inicial

---
+++ index.js
@@ -0,0 +1,8 @@
+const name = "Juan David";
+const lastname = "Castro Gallego";
+const completeName = (name, lastname) => {
+	let data1 = `${name} + ${lastname}`
+}
+const nickname = "juandc";
+
+console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
