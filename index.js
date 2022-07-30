8e6938e7600aab9b1d79794fcc56c3b67f1e06ac
siendomiguel <siendomigueldev@gmail.com>
2022-07-30 15:10:40 -0500

    agregando funcionalidad de edicion en el frontend

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-const name = "Juan David";
-const lastname = "Castro Gallego";
-const completeName = (name, lastname) => {
-	let data1 = `${name} + ${lastname}`
-}
-const nickname = "juandc";
+let nametwitter = document.querySelector('#name');
+let username = document.querySelector('#username');
+let date = document.querySelector('#date');
+let contain = document.querySelector('#text-contain');
+let hashtags = document.querySelector('#hashtags');
 
-console.log("Mi nombre es " + completeName + ", pero prefiero que me digas " + nickname + ".");
+let inputName = document.querySelector('#inputName');
+let inputUsername = document.querySelector('#inputUsername');
+let inputDate = document.querySelector('#inputDate');
+let inputContain = document.querySelector('#inputContain');
+let inputHashtags = document.querySelector('#inputHashtags');
+
+let btn = document.querySelector('#btn');
+
+// --------------------------
+
+btn.addEventListener('click', modificar);
+
+function modificar () {
+	nametwitter.innerText = inputName.value;
+	username.innerText = '@' + inputUsername.value;
+	date.innerText = '· ' + inputDate.value + '.';
+	contain.innerText = inputContain.value;
+	hashtags.innerText = inputHashtags.value;
+}
+
+
+
+/* // Obtener fecha actual
+
+
+function formatoFecha(fecha, formato) {
+    const map = {
+        dd: fecha.getDate(),
+        mm: fecha.getMonth() + 1,
+        yy: fecha.getFullYear().toString().slice(-2),
+        yyyy: fecha.getFullYear()
+    }
+
+    return formato.replace(/dd|mm|yy|yyy/gi, matched => map[matched])
+}
+
+let hoy = new Date();
+
+console.log(`${formatoFecha(hoy, 'dd/mm')}`);
+
+// Modificar atributo value en fecha
+
+inputDate.getAttribute('value','hola' ) */
